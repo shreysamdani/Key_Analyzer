36d7bf19e526522857d67411ba2c8730d484acf2
adityavarshney <adityavarshney@gmail.com>
2017-06-08 09:24:47 -0700

    updating setup and windows build

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,11 @@
 from cx_Freeze import setup, Executable
+import sys
 
-setup( name = "" , version = "0.1" , description = "Creates the Analyzer workbook and the template Python file" , executables = [Executable("Analyzer.py")] , options = {
+base = None
+if sys.platform == 'win32':
+	base = 'Win32GUI'
+
+setup( name = "" , version = "0.1" , description = "Creates the Analyzer workbook and the template Python file" , executables = [Executable("Analyzer.py", base = "Win32GUI")] , options = {
         "build_exe" : {
         	"packages": ['pandas', 'xlsxwriter', 'numpy'],
             "excludes": ['tcl', 'ttk', 'tkinter', 'Tkinter'],
